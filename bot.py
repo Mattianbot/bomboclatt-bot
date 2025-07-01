1c7e9e562f567f0ee52c0d8ebf64f8645a158756
Mattianbot <mattian174@gmail.com>
2025-07-01 23:52:55 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,16 @@
 import os
 import asyncio
-from telegram import Update, ChatAction
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Scrivimi qualcosa... 😎")
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, MessageHandler, filters
 
 async def bomboclatt_reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_chat_action(chat_id=update.effective_chat.id, action=ChatAction.TYPING)
-    await asyncio.sleep(1.5)
-    await update.message.reply_text("Bomboclatt")
+    await update.message.reply_text("bomboclatt")
 
 def main():
     BOT_TOKEN = os.getenv("BOT_TOKEN")
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
-    app.add_handler(CommandHandler("start", start))
+    # Handler per tutti i messaggi di testo (esclusi comandi)
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, bomboclatt_reply))
 
     print("Bomboclatt bot is online.")
@@ -23,4 +18,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
